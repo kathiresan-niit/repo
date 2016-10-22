cfa61a3477f5cf05028d9d1e783b84942a54b9bb
kathiresan-niit <n.kathiresan@niit.com>
2016-10-22 15:55:29 +0530

    done in master

---
+++ HomeController.java
@@ -0,0 +1,13 @@
+package com.techsoft.info.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcome(){
+		return "blogOperations";
+	}
+}
